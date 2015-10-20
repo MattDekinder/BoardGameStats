4318688662872d36ec9f0a381df1258850749e13
MattDekinder <dekinder@ualberta.ca>
2015-10-19 23:09:12 -0600

    added player model and a list adapter for it. The app crashes when a player is selected from the list. Need to add a checkbox to the xml and an attribute in player model to support multiple selection of player models.

---
+++ app/src/main/java/com/example/matt/boardgamestats/ListAdapterPlayerModel.java
@@ -0,0 +1,70 @@
+package com.example.matt.boardgamestats;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Matt on 8/16/2015.
+ */
+
+public class ListAdapterPlayerModel extends BaseAdapter {
+    ArrayList<PlayerModel> playerDataModel;
+    private LayoutInflater inflater;
+
+    @Override
+    public long getItemId(int position)
+    {
+
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+
+        return playerDataModel.get(position);
+    }
+
+    @Override
+    public int getCount()
+    {
+        if (playerDataModel == null){
+            return 0;
+        }
+        else{
+            return playerDataModel.size();
+        }
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+
+        if (convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.player_object, null);
+        }
+        TextView text1 = (TextView) convertView.findViewById(R.id.playerName);
+        text1.setText(playerDataModel.get(position).getName());
+        //text2.setText(Integer.toString(dataModel.get(position).getScore()));
+
+        return convertView;
+    }
+
+    public ListAdapterPlayerModel(Context context, ArrayList<PlayerModel> playerDataList)
+    {
+
+        super();
+        this.inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.playerDataModel = playerDataList;
+
+    }
+}
